2ec601163ddfd22f17c2e5ba48fb0a5f5be6eb85
Maxi.Rod <maxi.180@hotmail.com>
2020-07-29 16:13:43 -0300

    Cambios
    Modifique el style y el script logr agregar los ID y un elemento nuevo

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-/*let elemP = document.getElementsByTagName('p')[0];
-var attId = document.createAttribute('id');
-attId.value = "primero";
-elemP.appendChild(attId);
-elemP[0].setAttribute('id',"primero");*/
 alert("Prueba de alerta");
-document.addEventListener('DOMContentLoaded', function(){
-let elemP = document.getElementsByTagName('p')[0]
-elemP[0].setAttribute('id',"primero")
-//elemP[1].setAttribute('id', "segundo")    
+document.addEventListener('DOMContentLoaded', function () {
+    let newP = document.createElement('p');
+    newP.setAttribute('id', "info");
+    newP.innerHTML = "<strong>Hola</strong> ese parrafo <span>es una prueba de DOM</span> para mirar la entrada de elementos desde js Saludos!"
+    let elemP = document.getElementsByTagName('p');
+    elemP[0].setAttribute('id', "primero");
+    elemP[1].setAttribute('id', "segundo");
+    document.body.appendChild(newP)
 }
 
 )
